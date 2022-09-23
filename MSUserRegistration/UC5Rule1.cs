e0c3975276d3ebcbb54b91696323def64d30bf7f
unknown <Komal>
2022-09-24 01:13:39 +0530

    [Komal] Rule 1

---
+++ MSUserRegistration/UC5Rule1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MSUserRegistration
+{
+    public class UC5Rule1
+    {
+        public string password;
+
+        public UC5Rule1(string password)
+        {
+            this.password = password;
+        }
+
+        public string ValidatePassword()
+        {
+            var hasLowerChar = new Regex("^(?=.*[a-z]).{8,}$");
+            if (!hasLowerChar.IsMatch(password))
+            {
+                Console.WriteLine("password should have minimum 8 characters");
+                return null;
+            }
+            else
+            {
+                //Console.WriteLine("Password is valid");
+                return password;
+            }
+        }
+    }
+}
